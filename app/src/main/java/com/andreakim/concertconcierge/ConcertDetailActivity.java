a3babe7f71578c99f778c7481bb71447500cde25
ericaschulz <ericalynnschulz@gmail.com>
2016-07-25 16:31:46 -0500

    update

--- app/src/main/java/com/andreakim/concertconcierge/ConcertDetailActivity.java
+++ app/src/main/java/com/andreakim/concertconcierge/ConcertDetailActivity.java
@@ -1,13 +1,32 @@
 package com.andreakim.concertconcierge;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.net.URL;
+import java.net.URLEncoder;
 
 public class ConcertDetailActivity extends AppCompatActivity {
+
+    TextView mWeathertxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_concert_detail);
+
+
+    }
+    private URL createURL(String city) {
+        String apiKey = getString(R.string.openweather_api);
+        String baseUrl = getString(R.string.openweather_url);
+        try {
+            String url = baseUrl + URLEncoder.encode(city, "UTF-8") + "&units=imperial&cnt=16&APPID=" + apiKey;
+            return new URL(url);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return null;
     }
 }
