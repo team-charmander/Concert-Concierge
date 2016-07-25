f314db11e8e8c9c26ab89c8f8cb5ef8be144774c
Andrea Kim <andreaforanimals@gmail.com>
2016-07-25 14:40:49 -0500

    first commit after coding strted - basic layout

---
+++ app/src/main/java/com/andreakim/concertconcierge/Concert.java
@@ -0,0 +1,51 @@
+package com.andreakim.concertconcierge;
+
+/**
+ * Created by andreakim on 7/25/16.
+ */
+public class Concert {
+    String name;
+    String venue;
+    String date;
+
+    String time;
+
+    public Concert(String name, String venue, String date, String time) {
+        this.name = name;
+        this.venue = venue;
+        this.date = date;
+        this.time = time;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getVenue() {
+        return venue;
+    }
+
+    public void setVenue(String venue) {
+        this.venue = venue;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
---
+++ app/src/main/java/com/andreakim/concertconcierge/MainActivity.java
@@ -0,0 +1,33 @@
+package com.andreakim.concertconcierge;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+
+}
