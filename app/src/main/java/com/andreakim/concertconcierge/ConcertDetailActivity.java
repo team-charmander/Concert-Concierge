8eac0125e9d081010fd6312a7a24515fc2d904d6
Spoorthi Pendyala <pnks.234@gmail.com>
2016-07-28 12:44:06 -0500

    test

--- app/src/main/java/com/andreakim/concertconcierge/ConcertDetailActivity.java
+++ app/src/main/java/com/andreakim/concertconcierge/ConcertDetailActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class ConcertDetailActivity extends AppCompatActivity {
 
+    String name;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
