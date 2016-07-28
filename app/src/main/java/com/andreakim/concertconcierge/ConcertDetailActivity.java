e97be2afaf553fa3c1d0356e9b043c57772e6b57
ericaschulz <ericalynnschulz@gmail.com>
2016-07-28 14:33:06 -0500

    update

--- app/src/main/java/com/andreakim/concertconcierge/ConcertDetailActivity.java
+++ app/src/main/java/com/andreakim/concertconcierge/ConcertDetailActivity.java
@@ -2,16 +2,9 @@ package com.andreakim.concertconcierge;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
-
-import java.util.ArrayList;
 
 
 public class ConcertDetailActivity extends AppCompatActivity {
-
-    private RecyclerView recyclerView;
-    private WeatherAdapter weatherAdapter;
-    static private ArrayList<Weather> list_weathers;
 
 
     @Override
@@ -22,38 +15,3 @@ public class ConcertDetailActivity extends AppCompatActivity {
 
     }
 }
-
-//    private class WeatherTask extends AsyncTask<Void, Void, Void> {
-//
-////        int id = 0;
-//
-//        private TextView mTempView;
-//
-//
-//        public WeatherTask(TextView mTempView) {
-//
-//            this.mTempView = mTempView;
-//        }
-//
-//
-//        @Override
-//        protected Void doInBackground(Void... params) {
-//
-//            JSONObject jsonObject = JsonParser.getWeatherTextFromApi();
-//            if (jsonObject != null) {
-//                if (jsonObject.length() > 0) {
-//                    try {
-//                        JSONArray jsonArray = jsonObject.getJSONObject("temp").getJSONObject("name").getJSONArray("weather");
-//                        id = jsonArray.getJSONObject(0).getJSONObject("weather").getInt("id");
-//
-//                    } catch (JSONException e) {
-//                        e.printStackTrace();
-//                    }
-//                }
-//            }
-//
-//
-//            return null;
-//        }
-//    }
-//}
